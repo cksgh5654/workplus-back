a0c50d2038336b6e7ead47d9ca3458d10def9692
DonggunLim <Ldonggun6766@gmail.com>
2024-11-26 15:56:52 +0900

    chore: set controllers api router

---
+++ controllers/index.js
@@ -0,0 +1,3 @@
+const apiController = require("express").Router();
+
+module.exports = apiController;
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { PORT } = require("./consts/app");
+const apiController = require("./controllers");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/apit", apiController);
 
 app.listen(PORT, () => {
   console.log(`Express Running on ${PORT}`);
