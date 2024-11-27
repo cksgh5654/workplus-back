c030b242d4eac29e38cdec1cf90bffa24a3b9770
DonggunLim <Ldonggun6766@gmail.com>
2024-11-27 14:58:09 +0900

    fix: api controller router 오타 수정

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const { PORT } = require("./consts/app");
 const apiController = require("./controllers");
+const User = require("./schemas/user.schema");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/apit", apiController);
+app.use("/api", apiController);
 
 app.listen(PORT, () => {
   console.log(`Express Running on ${PORT}`);
