b2593e5f1bc2ef9442ac9cfee2da0481750f7731
DonggunLim <Ldonggun6766@gmail.com>
2024-11-27 16:27:38 +0900

    feat: add vacation schema

---
+++ schemas/vacation.schema.js
@@ -0,0 +1,31 @@
+const mongoose = require("../db_init");
+const { String } = mongoose.Schema.Types;
+
+const vacationSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    startDate: {
+      type: String,
+      required: true,
+    },
+    endDate: {
+      type: String,
+      required: true,
+    },
+    vacationType: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: true,
+    },
+  }
+);
+
+const Vacation = mongoose.model("vacations", vacationSchema);
+module.exports = Vacation;
