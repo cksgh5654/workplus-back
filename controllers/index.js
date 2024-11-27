c6303e16606ffbce9ca77d82572f559087d9d00a
DonggunLim <Ldonggun6766@gmail.com>
2024-11-27 16:45:35 +0900

    feat: add authController

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
+const authController = require("./auth.controller");
 const userController = require("./user.contorller");
 const apiController = require("express").Router();
 
 apiController.use("/user", userController);
+apiController.use("/auth", authController);
 
 module.exports = apiController;
