c4356f0a067767b2ede18b0f3e6739f37b4ed04d
DonggunLim <Ldonggun6766@gmail.com>
2024-11-28 00:49:49 +0900

    chore: mongoDB workplus database에 연결

--- db_init.js
+++ db_init.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const { MONGO_DB_URL } = require("./consts/app");
 
 mongoose
-  .connect(MONGO_DB_URL)
+  .connect(MONGO_DB_URL, { dbName: "workplus" })
   .then(() => console.log("MongoDB Connected"))
   .catch((error) => console.log("fail to connect mongoDB", error));
 
