777c9beec9127e5217f1aabf27efd76e7f6df29f
DonggunLim <Ldonggun6766@gmail.com>
2024-11-27 16:32:21 +0900

    feat: add userController

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,6 @@
+const userController = require("./user.contorller");
 const apiController = require("express").Router();
+
+apiController.use("/user", userController);
 
 module.exports = apiController;
---
+++ controllers/user.contorller.js
@@ -0,0 +1,3 @@
+const userController = require("express").Router();
+
+module.exports = userController;
