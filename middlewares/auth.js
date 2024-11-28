af12b039408cd8ee030f6daa53ce0905ec020e2d
DonggunLim <Ldonggun6766@gmail.com>
2024-11-28 20:04:03 +0900

    chore: clear consolelog

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,15 +4,12 @@ const { findUserByEmail } = require("../services/user.service");
 
 const withAuth = async (req, res, next) => {
   const token = req.headers.authorization;
-  console.log({ token });
   if (!token) {
     return res
       .status(400)
       .json({ isError: true, message: "헤더에 토큰이 필요합니다." });
   }
   const { email, exp: expires } = jwt.verify(token, JWT_SECRET_KEY);
-  console.log({ expires });
-  console.log(Date.now() / 1000);
   try {
     const user = await findUserByEmail({ email });
     if (!user) {
