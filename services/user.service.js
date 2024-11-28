f1f44114fb82537d0130b44c208ac601b0e67e5c
DonggunLim <Ldonggun6766@gmail.com>
2024-11-28 12:51:51 +0900

    feat: findUserByEmail, updateUser 함수추가

--- services/user.service.js
+++ services/user.service.js
@@ -5,22 +5,44 @@ const createUser = (userData) => {
     const document = User.create(userData);
     return document;
   } catch (error) {
-    console.log(error);
-    return false;
+    throw new Error(error);
   }
 };
 
 const findUserById = async (id) => {
   try {
-    const result = await User.findOne(id);
-    return result === null ? false : true;
+    const document = await User.findOne(id);
+    return !!document === null ? false : true;
   } catch (error) {
-    console.log(error);
-    return false;
+    throw new Error(error);
+  }
+};
+
+const findUserByEmail = async (email) => {
+  try {
+    const document = await User.findOne(email);
+    return document;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const updateUser = async (data) => {
+  const { email, ...rest } = data;
+  try {
+    const updated = await User.updateOne({ _id: id }, rest);
+    if (!updated) {
+      return null;
+    }
+    return updated;
+  } catch (error) {
+    throw new Error(error);
   }
 };
 
 module.exports = {
   createUser,
   findUserById,
+  findUserByEmail,
+  updateUser,
 };
