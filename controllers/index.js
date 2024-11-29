4898f916cf1f2cc1afd543814620bae4ccadd208
DonggunLim <Ldonggun6766@gmail.com>
2024-11-29 10:46:34 +0900

    feat: meeting controller, service file 추가

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 const authController = require("./auth.controller");
+const meetingController = require("./meeting.controller");
 const userController = require("./user.contorller");
 const vacationController = require("./vacation.controller");
 const apiController = require("express").Router();
@@ -6,5 +7,6 @@ const apiController = require("express").Router();
 apiController.use("/user", userController);
 apiController.use("/auth", authController);
 apiController.use("/vacation", vacationController);
+apiController.use("/meeting", meetingController);
 
 module.exports = apiController;
