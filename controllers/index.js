4867249446922d78d800cb13170d78fd4d9e2145
DonggunLim <Ldonggun6766@gmail.com>
2024-11-29 19:42:40 +0900

    fix: user controller 경로 수정

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const withAuth = require("../middlewares/auth");
 const authController = require("./auth.controller");
 const meetingController = require("./meeting.controller");
-const userController = require("./user.contorller");
+const userController = require("./user.controller");
 const vacationController = require("./vacation.controller");
 const apiController = require("express").Router();
 
