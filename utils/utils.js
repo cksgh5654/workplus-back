ef9a40f2d3dc473c80806d1e70d3ea5d51066996
DonggunLim <Ldonggun6766@gmail.com>
2024-12-03 19:19:56 +0900

    fix: userimage 없을때 빈값으로

--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,9 @@
 const { BASE_URL } = require("../consts/app");
 
 const processImageUrl = (url) => {
+  if (!url.length) {
+    return "";
+  }
   const isGoolgeProvide = url.startsWith("https://lh3.googleusercontent.com");
   return isGoolgeProvide ? url : `${BASE_URL}/${url}`;
 };
