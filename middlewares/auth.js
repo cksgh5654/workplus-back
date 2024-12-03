8f26a654b6180cbb3d54a0d07c4c6bdda09bb3f5
DonggunLim <Ldonggun6766@gmail.com>
2024-12-03 19:35:40 +0900

    fix: 코드리뷰 auth 상태코드 수정, verify 예외처리 event 이용

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,27 +6,37 @@ const withAuth = async (req, res, next) => {
   const token = req.headers.authorization;
   if (!token) {
     return res
-      .status(400)
+      .status(401)
       .json({ isError: true, message: "헤더에 토큰이 필요합니다." });
   }
-  const { email, exp: expires } = jwt.verify(token, JWT_SECRET_KEY);
+
   try {
+    const { email } = jwt.verify(token, JWT_SECRET_KEY);
     const user = await findUserByEmail({ email });
     if (!user) {
       return res
-        .status(400)
+        .status(401)
         .json({ isError: true, message: "토큰이 올바르지 않습니다." });
-    }
-
-    if (expires < Date.now() / 1000) {
-      return res
-        .status(400)
-        .json({ isError: true, message: "토큰이 만료되었습니다." });
     }
 
     next();
   } catch (error) {
-    return res.status(500).json({ isError: true, message: error.message });
+    const { name } = error;
+    if (name === "TokenExpiredError") {
+      return res
+        .status(401)
+        .json({ isError: true, message: "토큰이 만료되었습니다." });
+    }
+
+    if (name === "JsonWebTokenError") {
+      return res
+        .status(401)
+        .json({ isError: true, message: "토큰이 올바르지 않습니다." });
+    }
+
+    return res
+      .status(500)
+      .json({ isError: true, message: "토큰을 가져오지 못했습니다." });
   }
 };
 
